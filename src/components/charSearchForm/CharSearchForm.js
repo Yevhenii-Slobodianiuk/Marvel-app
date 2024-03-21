302077d32152a171a3791af15635627c076213b5
Yevhenii <evgeniy.slobod9@gmail.com>
2024-03-21 15:39:44 +0200

    created component CharSearchForm

---
+++ src/components/charSearchForm/CharSearchForm.js
@@ -0,0 +1,38 @@
+import { Formik, Field, Form, ErrorMessage } from "formik"
+
+import './charSearchForm.scss';
+
+const CharSearchForm = () => {
+
+	return (
+		<div className="char__search-form">
+			<Formik
+				initialValues={{
+					charName: "",
+				}}
+				onSubmit={values => console.log(JSON.stringify(values, null, 2))}
+			>
+				<Form>
+					<label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
+					<div className="char__search-wrapper">
+						<Field
+							id="charName"
+							name='charName'
+							type='text'
+							placeholder="Enter name" />
+						<button
+							type='submit'
+							className="button button__main"
+						// disabled={loading}
+						>
+							<div className="inner">find</div>
+						</button>
+					</div>
+					<ErrorMessage className="char__search-error" name="charName" component="div" />
+				</Form>
+			</Formik>	
+		</div>
+	)
+}
+
+export default CharSearchForm;
